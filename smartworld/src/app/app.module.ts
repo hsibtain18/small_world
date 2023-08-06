cb6b5709e659cd4f20fd35a66f449cb344a06688
syedHassanSIbtain <hsibtain18@gmail.com>
2023-08-07 00:02:16 +0500

    working commit

--- smartworld/src/app/app.module.ts
+++ smartworld/src/app/app.module.ts
@@ -4,17 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { TopNavComponent } from './shared/top-nav/top-nav.component';
+import { SideNavComponent } from './shared/side-nav/side-nav.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpService } from './shared/http.service';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    SideNavComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatFormFieldModule, MatTableModule, MatSortModule, MatPaginatorModule
   ],
-  providers: [],
+  providers: [
+    HttpService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ smartworld/src/app/main-content/main-content.component.ts
@@ -0,0 +1,63 @@
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { HttpService } from '../shared/http.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+export interface UserData {
+  id: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
+@Component({
+  selector: 'app-main-content',
+  templateUrl: './main-content.component.html',
+  styleUrls: ['./main-content.component.scss']
+})
+export class MainContentComponent implements OnInit ,AfterViewInit {
+  dataSource!: MatTableDataSource<UserData>;
+  displayedColumns: string[] = ['id', 'first_name', 'last_name', 'avatar'];
+  totalRecords = 0;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  currentPage = 0
+  pageSize =0;
+  constructor(private http: HttpService) {
+
+  }
+  ngOnInit(): void {
+    this.searchAllUser(this.currentPage);
+  }
+  searchAllUser(page: number) {
+    this.http.getAllUsers(page)
+      .subscribe((val: any) => {
+        console.log(val);
+        this.totalRecords = val.total
+        this.dataSource = new MatTableDataSource(val.data);
+        this.paginator.pageIndex = this.currentPage;
+        this.paginator.length = val.total;
+      })
+
+   
+  }
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    }, 100);
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  pageChanged(event: PageEvent) {
+    console.log({ event });
+    this.pageSize = event.pageSize;
+    this.currentPage = event.pageIndex;
+    this.searchAllUser(this.currentPage);
+  }
+}
